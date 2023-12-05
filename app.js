777224dd66a43529e67016d30b6f6c8aff4b2fba
Pencho Ivanov <penchoivanov@Penchos-Air.home>
2023-12-05 21:44:23 +0100

    2pages-css

--- app.js
+++ app.js
@@ -4,30 +4,27 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const SpotifyWebApi = require('spotify-web-api-node');
-
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
-
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
 });
-
-// console.log('Client ID:', process.env.CLIENT_ID);
-// console.log('Client ID:', process.env.CLIENT_SECRET);
 
 spotifyApi
   .clientCredentialsGrant()
   .then(data => spotifyApi.setAccessToken(data.body['access_token']))
   .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
+
+// pages view
+
 app.get('/', (req,res) => {
-    res.render('index',{title: "homepage", bgPage: "homePage"})
+    res.render('index', {title: "homepage", bgPage: "homePage"})
 })
 
 app.get('/artistSearch', (req, res) => {
@@ -35,10 +32,10 @@ app.get('/artistSearch', (req, res) => {
 
     spotifyApi.searchArtists(artistQuery)
             .then(data => {
-                //console.log('The received data from the API: ', data.body.artists.items[0]);
-                res.render('artistSearch', {artists: data.body.artists.items});
+                console.log('The received data from the API: ', data.body.artists.items[0]);
+                res.render('artistSearch', {artists: data.body.artists.items, title: "search", bgPage: "search-results"});
             })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 });
 
 app.get('/albums/:id' ,(req,res) => {
